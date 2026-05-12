121f1e5a0b1bd53656ee1e88c38d757b92f40f50
Vela Juarez <vovazms@gmail.com>
2026-05-12 19:56:00 +0300

    Init vec3

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -1,5 +1,19 @@
-//
-// Created by Владимир on 11.05.2026.
-//
-
 #pragma once
+#include <utility>
+
+#include "math_concepts.h"
+
+namespace zem::math {
+template <typename T>
+  requires MathScalar<T>
+struct Vec3 {
+  T x{0};
+  T y{0};
+  T z{0};
+
+  Vec3() = default;
+  Vec3(T x, T y, T z) noexcept : x(x), y(y), z(z) {}
+
+  friend bool operator==(const Vec3&, const Vec3& rhs) noexcept = default;
+};
+}  // namespace zem::math
