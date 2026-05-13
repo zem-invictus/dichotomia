3ab8f0bee3fb5f234229cc4b59306800b21866fd
vladimir <vovazms@gmail.com>
2026-05-13 14:24:22 +0300

    Added Vec3

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -1,19 +1,122 @@
 #pragma once
-#include <utility>
+#include <cmath>
 
+#include "math_assert.h"
 #include "math_concepts.h"
 
 namespace zem::math {
-template <typename T>
-  requires MathScalar<T>
+template <MathScalar T>
 struct Vec3 {
   T x{0};
   T y{0};
   T z{0};
 
-  Vec3() = default;
-  Vec3(T x, T y, T z) noexcept : x(x), y(y), z(z) {}
+  [[nodiscard]] friend constexpr bool operator==(Vec3, Vec3) noexcept = default;
 
-  friend bool operator==(const Vec3&, const Vec3& rhs) noexcept = default;
+  constexpr Vec3& operator+=(Vec3 rhs) noexcept {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    return *this;
+  }
+
+  constexpr Vec3& operator-=(Vec3 rhs) noexcept {
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    return *this;
+  }
+
+  constexpr Vec3& operator*=(T scalar) noexcept {
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    return *this;
+  }
+
+  constexpr Vec3& operator/=(T scalar) noexcept {
+    ZEM_MATH_EXPECTS(scalar != T{0});
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    return *this;
+  }
+
+  [[nodiscard]] friend constexpr Vec3 operator+(Vec3 lhs, Vec3 rhs) noexcept {
+    lhs += rhs;
+    return lhs;
+  }
+
+  [[nodiscard]] friend constexpr Vec3 operator-(Vec3 lhs, Vec3 rhs) noexcept {
+    lhs -= rhs;
+    return lhs;
+  }
+
+  [[nodiscard]] friend constexpr Vec3 operator*(Vec3 lhs, T scalar) noexcept {
+    lhs *= scalar;
+    return lhs;
+  }
+
+  [[nodiscard]] friend constexpr Vec3 operator*(T scalar, Vec3 lhs) noexcept {
+    lhs *= scalar;
+    return lhs;
+  }
+
+  [[nodiscard]] friend constexpr Vec3 operator/(Vec3 lhs, T scalar) noexcept {
+    lhs /= scalar;
+    return lhs;
+  }
+
+  [[nodiscard]] constexpr Vec3 operator-(this Vec3 self) noexcept {
+    return {-self.x, -self.y, -self.z};
+  }
+
+  [[nodiscard]] constexpr T Dot(this Vec3 self, Vec3 rhs) noexcept {
+    return self.x * rhs.x + self.y * rhs.y + self.z * rhs.z;
+  }
+
+  [[nodiscard]] constexpr Vec3 Cross(this Vec3 self, Vec3 rhs) noexcept {
+    return {.x = self.y * rhs.z - self.z * rhs.y,
+            .y = self.z * rhs.x - self.x * rhs.z,
+            .z = self.x * rhs.y - self.y * rhs.x};
+  }
+
+  [[nodiscard]] T Length(this Vec3 self) noexcept
+    requires std::floating_point<T>
+  {
+    return std::sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
+  }
+
+  [[nodiscard]] constexpr T LengthSquared(this Vec3 self) noexcept {
+    return self.x * self.x + self.y * self.y + self.z * self.z;
+  }
 };
+
+using Vec3i = Vec3<int>;
+using Vec3f = Vec3<float>;
+using Vec3d = Vec3<double>;
+using Vec3u = Vec3<unsigned int>;
+
+static_assert(std::is_trivially_copyable_v<Vec3i>,
+              "Vec3 implementation is broken!");
+static_assert(std::is_trivially_copyable_v<Vec3f>,
+              "Vec3 implementation is broken!");
+static_assert(std::is_trivially_copyable_v<Vec3d>,
+              "Vec3 implementation is broken!");
+static_assert(std::is_trivially_copyable_v<Vec3u>,
+              "Vec3 implementation is broken!");
+
+static_assert(sizeof(Vec3i) == sizeof(int) * 3, "Vec3i has padding issues!");
+static_assert(alignof(Vec3i) == alignof(int), "Vec3i alignment is broken!");
+
+static_assert(sizeof(Vec3f) == sizeof(float) * 3, "Vec3f has padding issues!");
+static_assert(alignof(Vec3f) == alignof(float), "Vec3f alignment is broken!");
+
+static_assert(sizeof(Vec3d) == sizeof(double) * 3, "Vec3d has padding issues!");
+static_assert(alignof(Vec3d) == alignof(double), "Vec3d alignment is broken!");
+
+static_assert(sizeof(Vec3u) == sizeof(unsigned int) * 3,
+              "Vec3u has padding issues!");
+static_assert(alignof(Vec3u) == alignof(unsigned int),
+              "Vec3u alignment is broken!");
 }  // namespace zem::math
