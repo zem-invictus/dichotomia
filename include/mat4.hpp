b2b7da7177d342918a2516f2a87172117ae8116c
vladimir <vovazms@gmail.com>
2026-05-16 22:22:12 +0300

    Mat4 WIP

--- include/mat4.hpp
+++ include/mat4.hpp
@@ -1,9 +1,21 @@
 #pragma once
+#include <vector>
+
 #include "math_concepts.h"
+#include "vec4.hpp"
 
 namespace zem::math {
-template<MathScalar T>
+template <MathScalar T>
 struct Mat4 {
+  Vec4<T> cols[4];
 
+  [[nodiscard]] static constexpr Mat4& Identity() noexcept {
+    return {.cols = {{T{1}, T{0}, T{0}, T{0}},
+                     {T{0}, T{1}, T{0}, T{0}},
+                     {T{0}, T{0}, T{1}, T{0}},
+                     {T{0}, T{0}, T{0}, T{1}}}};
+  }
+
+  
 };
-}
+}  // namespace zem::math
--- include/zem_math.hpp
+++ include/zem_math.hpp
@@ -3,3 +3,4 @@
 #include "vec2.hpp"
 #include "vec3.hpp"
 #include "vec4.hpp"
+#include "mat4.hpp"
